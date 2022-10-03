59a1f04ccf3c4b98cb12d41623626e8d78340562
sonnysam <samuelagbenyo067@gmail.com>
2022-10-03 10:58:20 +0000

    about fixed

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -13,30 +13,36 @@ const About = () => {
     }
 
   return (
-    <section className='mt-24 mx-3'>
-        <div className='grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2'>
-            <div>
-                <img
-                 className='lg:h-[80vh] md:flex lg:flex hidden'
-                 src={image} alt="" />
-            </div>
-            <div>
-                <strong className='text-3xl text-center'>ABOUT US</strong>
-                <p className='flow-root ... max-w-md text-gray-600'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi porro modi molestias? Non, nobis aut delectus ea molestias reiciendis ipsam, a corrupti quibusdam accusantium distinctio vero numquam cupiditate aliquid ipsum?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, iure doloremque! Atque illum nobis, ullam ipsum provident quae earum corrupti quisquam at perferendis facere, eius repellat doloribus placeat quod possimus.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos porro molestias nostrum aut iste. Eveniet iusto at nam deserunt id dolore, placeat minima numquam doloremque vel minus dicta autem sequi!
-                </p>
-                <button 
-                    onClick={handleSubmit}
-                    className='bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded'
-                    type='button'>
-                    READ MORE
-                </button>
-            </div>
+    <section className="mt-24 mx-3">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2">
+        <div>
+          <img
+            className="lg:h-[50vh] lg:w-[30vw] md:flex lg:flex hidden"
+            src={image}
+            alt=""
+          />
         </div>
+        <div>
+          <strong className="text-3xl text-center">ABOUT US</strong>
+          <p className="flow-root ... max-w-md text-gray-600 mt-3">
+            Vision Board Realty Limited (VBRL) is an organisation committed and
+            dedicated to assisting our clients, both local and abroad, find
+            their ideal homes and properties. <br /> Our focus is to ensure that
+            the purchasing, rental or management process is seamless for our
+            clients by providing options that meet their needs while remaining
+            professionals and delivering excellence.
+          </p>
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-500 hover:bg-blue-900 mt-5 text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >
+            READ MORE
+          </button>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default About
--- src/components/vision/VisionBoard.jsx
+++ src/components/vision/VisionBoard.jsx
@@ -13,31 +13,19 @@ const VisionBoard = () => {
         <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2">
           <div>
             <img
-              className="lg:h-[70vh] md:flex lg:flex hidden mx-auto"
+              className="lg:h-[50vh] lg:w-[30vw] md:flex lg:flex hidden mx-auto"
               src={image}
               alt=""
             />
           </div>
           <div>
-            <p className="flow-root ...  max-w-lg text-gray-600 mx-3">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo,
-              eius optio! Placeat nesciunt eos excepturi aut consectetur magni,
-              hic doloremque modi, repudiandae ad sequi libero ut quam vero a
-              distinctio maiores laboriosam iste explicabo ea? Aut tenetur
-              incidunt quos voluptatibus autem delectus recusandae laboriosam
-              quas harum id laborum assumenda ipsam tempora iure, nulla
-              exercitationem alias explicabo aliquid quasi illum a quae
-              quibusdam! Dolores adipisci tempora provident vitae, harum
-              assumenda velit. Fuga aperiam officia quidem, saepe natus ipsa eum
-              laudantium. Quia libero, architecto aperiam, voluptate quasi alias
-              ullam sunt tempore, labore magnam cupiditate earum incidunt a
-              aspernatur impedit sint illo? Modi aspernatur laboriosam
-              accusantium vero voluptatibus, aliquid quas fugit? Autem odit
-              asperiores atque beatae. Accusamus similique nam pariatur laborum
-              totam nobis quae blanditiis reiciendis, corrupti provident
-              voluptates! Culpa eos maxime placeat, quas beatae eaque nihil
-              reprehenderit vel illum tempora dolor distinctio sunt, error ea
-              doloremque. Assumenda iusto neque repellendus impedit fuga.
+            <p className="flow-root ...  max-w-lg text-gray-600 mx-3 mt-10">
+              Vision Board Realty Limited (VBRL) is an organisation committed
+              and dedicated to assisting our clients, both local and abroad,
+              find their ideal homes and properties. <br /> Our focus is to
+              ensure that the purchasing, rental or management process is
+              seamless for our clients by providing options that meet their
+              needs while remaining professionals and delivering excellence.
             </p>
           </div>
         </div>
@@ -48,29 +36,16 @@ const VisionBoard = () => {
           <div className=" text-center mx-auto">
             <img src={vision} alt="" className="mx-auto v" />
             <strong className="g font-bold">Our Vision</strong>
-            <p className="max-w-lg mx-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              similique expedita nostrum vitae commodi laudantium animi sapiente
-              rerum? Odio nihil explicabo, a dolorum cupiditate obcaecati
-              reiciendis expedita, eveniet corporis quisquam qui aperiam! In eum
-              vitae vero quasi, minima delectus similique facere! Iusto pariatur
-              eum dolorum earum, ipsum laboriosam! Hic, adipisci? Lorem ipsum
-              dolor sit amet, consectetur adipisicing elit. Ad corrupti modi,
-              impedit alias placeat error perferendis nobis amet.
+            <p className="max-w-lg mx-3 text-gray-500">
+              To be recognised as a trusted and reliable brokerage.
             </p>
           </div>
           <div className="text-center mx-auto mb-7">
             <img src={goal} alt="" className="mx-auto v" />
             <strong className="g font-bold">Our Mission</strong>
-            <p className="max-w-lg mx-3">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel
-              accusamus laboriosam pariatur ipsa sapiente enim beatae facere
-              dolorum asperiores voluptatem? Dolor illo ut earum veritatis nihil
-              dicta magni odit dolorum, quasi sit nesciunt tempora excepturi,
-              repellendus dolores. Quos id, quaerat aperiam error ratione
-              nostrum, eveniet pariatur maxime autem laboriosam iste! Lorem,
-              ipsum dolor sit amet consectetur adipisicing elit. Eos consequatur
-              culpa totam sit sed laborum quos soluta qui!
+            <p className="max-w-lg mx-3 text-gray-500">
+              To be excellent in service delivery while remaining professional
+              and maintaining international standards.
             </p>
           </div>
         </div>
