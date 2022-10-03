c37fab083f42d0d5631c7aadd14681d4d6046bf7
sonnysam <samuelagbenyo067@gmail.com>
2022-10-03 11:38:02 +0000

    testimony removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Hero from "../components/Home/Hero";
 import How from "../components/Home/How";
 import Partners from "../components/Home/Partners";
 import Property from "../components/Home/Property";
-import Testimony from "../components/Home/Testimony";
+// import Testimony from "../components/Home/Testimony";
 import Us from "../components/Home/Us";
 
 const Home = () => {
@@ -18,7 +18,7 @@ const Home = () => {
       <Property />
       <How/>
       <Cal />
-      <Testimony />
+      {/* <Testimony /> */}
     </div>
   );
 };
